00917c93bce15aa1615d2a415b5787aea657bbfa
John Vinh <john.michael.vinh2@gmail.com>
2022-12-13 21:53:10 -0500

    Parse input

--- day_13.py
+++ day_13.py
@@ -1,2 +1,6 @@
 with open('input.txt', 'r') as f:
     lines = [line.strip() for line in f.readlines()]
+
+for i in range(0, len(lines), 3):
+    item_1 = eval(lines[i])
+    item_2 = eval(lines[i + 1])
